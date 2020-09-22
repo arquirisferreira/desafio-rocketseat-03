07783df6c26f184a0cc68598125381c4acc1b1b6
Arquiris <arquiris@myia.com.br>
2020-09-22 18:21:26 -0300

    Desafio resolvido

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+    baseURL: 'http://10.0.2.2:3333',
 });
 
 export default api;
+
+/**
+ * IOS com emulador: localhost
+ * IOS com dispositivo fisico: ip da maquina
+ * Android com emulador: usando o adb reverse (localhost)
+ * Android com emulador: usar ip do emulador do android studio (10.0.2.2)
+ * Genymotio: 10.0.3.2
+ * Android Fisico: ip da maquina 
+ */
